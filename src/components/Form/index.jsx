af6c27852bd39e960580d1100d7aebcae6c7ab88
Brett Cadigan <brettcadigan@gmail.com>
2024-09-04 13:29:07 -0400

    Finished Inner Modal and signup

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,17 +1,73 @@
 import React from 'react'
+import * as Yup from 'yup'
+import {Formik, Form, Field, useField} from 'formik' 
+import {Input, Label, Error, Submit} from './styles'
 
-const FormComponent = ({handleSuccess}) => {
+const InputComponent = ({label, ...props}) => {
+  const [field, meta] = useField(props)
+
   return (
-    <form onSubmit={handleSuccess}>
-      <label for="name" >Name: </label>
-      <input name="name" type="text" required autoComplete="off" />
-
-      <label for="email">Email address: </label>
-      <input name="email" type="email" required autoComplete="off" />
-
-      <button type="submit">Submit</button>
-    </form>
+    <Label>
+      {label}: {meta.touched && meta.error && <Error>{meta.error}</Error>}
+      <Input {...field} {...props} />
+    </Label>
   )
 }
 
+const FormComponent = ({handleSuccess}) => {
+  const schema = Yup.object().shape({
+    name: Yup.string().required('Required field'),
+    email: Yup.string()
+              .email('Must be a valid email address')
+              .required('Required field')
+  })
+
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        email: ''
+      }}
+      onSubmit={handleSuccess}
+      validationSchema={schema}
+    >
+      {() => (
+        <Form>
+          <InputComponent name="name" type="type" label="Name" autoComplete="off" />
+          <InputComponent name="email" type="email" label="Email" autoComplete="off" />
+
+          <Submit type="submit">Submit</Submit>
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+
+
+          // <label>Name: </label>
+          // <Field name="name" type="text" autoComplete="off" />
+          // {touched.name && errors && <div>{errors.name}</div>}
+          
+          // <label>Email address: </label>
+          // <Field name="email" type="email" autoComplete="off" />
+          // {touched.email && errors && <div>{errors.email}</div>}
+
+          // <button type="submit">Submit</button>
+
+
+
+
+          //{props && <p>prooops</p>}
+//
+          // <InputComponent name="name" type="type" label="Name" />
+          // <InputComponent name="name" type="type" label="Name" />
+          //{props.touched.name && props.errors && <div>{props.errors.name}</div>}
+          //{props.touched.email && props.errors && <div>{props.errors.email}</div>}
+
+          // {touched.name && errors && <div>{errors.name}</div>}
+          // {touched.email && errors && <div>{errors.email}</div>}
+
+
 export default FormComponent
+  
--- src/components/ModalInner/index.jsx
+++ src/components/ModalInner/index.jsx
@@ -1,6 +1,32 @@
 import React, {useState} from 'react'
-import {Container} from './styles'
+import {Container, H3, H4, P, Socials, SuccessContainer} from './styles'
 import Form from '../Form'
+import {ReactComponent as Happy} from './happy_face.svg'
+import {ReactComponent as Instagram} from './icon_instagram.svg'
+import {ReactComponent as Facebook} from './icon_facebook.svg'
+import {ReactComponent as Twitter} from './icon_twitter.svg'
+
+const Input = ({handleSuccess}) => (
+  <Container>
+    <H3>Sign Up</H3>
+    <P>Join our newsletter to be notifed of when pre-release ticket for 
+    our first ever world tour go on sale.</P>
+    <Form handleSuccess={handleSuccess} />
+  </Container>
+)
+
+const Success = () => (
+  <SuccessContainer>
+    <Happy />
+    <H4>Yay! We are so excited!</H4>
+    <P>We hope you are too! In the meantime you can follow us on social media to keep up to date:</P>
+    <Socials>
+       <Instagram />
+       <Facebook />
+       <Twitter />
+    </Socials>
+  </SuccessContainer>
+)
 
 const ModalInner = () => {
   const [showSuccess, setShowSuccess] = useState(false)
@@ -9,27 +35,7 @@ const ModalInner = () => {
     setShowSuccess(true)
   }
 
-  return (
-    <Container>
-      {showSuccess ? (
-        <>
-          <h4>Yay! We are so excited!</h4>
-          <p>We hope you are too! In the meantime you can follow us on social media to keep up to date:</p>
-        </>
-      ) : (
-        <>
-          <h3>Sign Up</h3>
-          <p>Join our newsletter to be notifed of when pre-release ticket for 
-          our first ever world tour go on sale.</p>
-          <Form handleSuccess={handleSuccess} />
-        </>
-      )
-    }
-
-
-
-    </Container>
-  )
+  return showSuccess ? <Success /> : <Input handleSuccess={handleSuccess} />
 }
 
 export default ModalInner
